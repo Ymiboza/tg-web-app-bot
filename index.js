edd238b4218f7900e0b0f02449d8d34178734bf7
Gene Wagner <sprigan.uchiha@gmail.com>
2022-11-03 15:10:50 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,72 @@
+const TelegramBot = require("node-telegram-bot-api");
+const express = require("express");
+const cors = require("cors");
+
+const token = "5662715762:AAGO98e7vMYeHH6TR_vXn8BjItt2YQuCGyA";
+const webAppUrl = "https://9682-178-121-35-149.eu.ngrok.io/";
+const bot = new TelegramBot(token, { polling: true });
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+bot.on("message", async (msg) => {
+  const chatId = msg.chat.id;
+  const text = msg.text;
+
+  if (text !== "/start") {
+    await bot.sendMessage(chatId, "Write the /start", {});
+  }
+
+  if (text === "/start") {
+    await bot.sendMessage(chatId, "Press the button", {
+      reply_markup: {
+        inline_keyboard: [
+          [{ text: "Test store", web_app: { url: webAppUrl } }],
+        ],
+      },
+    });
+  }
+
+  if (msg?.web_app_data?.data) {
+    try {
+      const data = JSON.parse(msg?.web_app_data?.data);
+
+      await bot.sendMessage(chatId, "Thanks for the feedback");
+      await bot.sendMessage(chatId, "Your country: " + data?.country);
+      await bot.sendMessage(chatId, "Your street: " + data?.street);
+
+      setTimeout(async () => {
+        await bot.sendMessage(
+          chatId,
+          "You will receive all information in this chat"
+        );
+      }, 3000);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+});
+
+app.post("/web-data", async (req, res) => {
+  const { queryId, products = [], totalPrice } = req.body;
+  try {
+    await bot.answerWebAppQuery(queryId, {
+      type: "article",
+      id: queryId,
+      title: "Successful purchase",
+      input_message_content: {
+        message_text: ` Congratulations on your purchase, you have purchased an item worth ${totalPrice}, ${products
+          .map((item) => item.title)
+          .join(", ")}`,
+      },
+    });
+    return res.status(200).json({});
+  } catch (e) {
+    return res.status(500).json({});
+  }
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => console.log("server started on PORT " + PORT));
